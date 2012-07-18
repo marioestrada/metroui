77bd8aa4788caa4f3b323ef2451ab4b92fd87c76
Mario Estrada <mario.estrada@gmail.com>
2012-07-17 22:59:49 -0700

    sub menu top controls

--- js/metro.js
+++ js/metro.js
@@ -1,10 +1,24 @@
 $(function()
 {
-	$('#torrents').on('click', '.torrent', function()
+	$('#torrents').on('click', '.torrent', function(e)
 	{
+		e.preventDefault()
 		// $('.torrent', '#torrents').removeClass('selected')
 		$(this).toggleClass('selected')
 
 		$('#torrent_controls').toggleClass('open', $('.torrent.selected', '#torrents').length > 0)
+	})
+
+	$('#controls_top').on('click', '.sub > a:first', function(e)
+	{
+		e.preventDefault()
+		$(this).parent().toggleClass('open')
+		$(this).next('ul').css({
+			scale: 0.8,
+			opacity: 0
+		}).animate({
+			scale: 1,
+			opacity: 1
+		}, 200)
 	})
 })
