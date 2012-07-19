44ee915e87d7591e0d7013295c142f7b285b6192
Mario Estrada <mario.estrada@gmail.com>
2012-07-18 23:34:52 -0700

    Initial connection

--- js/metro.js
+++ js/metro.js
@@ -1,72 +1,89 @@
+window.btapp = new Btapp();
+btapp.connect();
+
+
+btapp.on('add:torrent', function(torrent_list) {
+    torrent_list.each(function(torrent)
+    {
+        console.log(torrent);
+    })
+});
+
+btapp.live('torrent * properties', function(properties, torrent, torrent_list)
+{
+    console.log(properties.arguments)
+})
+
 $(function()
 {
-	$('#torrents').on('click', '.torrent', function(e)
-	{
-		e.preventDefault()
-		// $('.torrent', '#torrents').removeClass('selected')
-		$(this).toggleClass('selected')
 
-		$('#torrent_controls').toggleClass('open', $('.torrent.selected', '#torrents').length > 0)
-	})
+    $('#torrents').on('click', '.torrent', function(e)
+    {
+        e.preventDefault()
+        // $('.torrent', '#torrents').removeClass('selected')
+        $(this).toggleClass('selected')
 
-	$('#controls_top').on('click', '.sub ul a', function(e)
-	{
-		e.preventDefault()
-		$(this).closest('.sub').removeClass('open')
-	})
+        $('#torrent_controls').toggleClass('open', $('.torrent.selected', '#torrents').length > 0)
+    })
 
-	$('#controls_top').on('click', '.sub > a:first', function(e)
-	{
-		e.preventDefault()
-		$(this).parent().toggleClass('open')
-		$(this).next('ul').css({
-			scale: 0.8,
-			opacity: 0
-		}).animate({
-			scale: 1,
-			opacity: 1
-		}, 200)
-	})
+    $('#controls_top').on('click', '.sub ul a', function(e)
+    {
+        e.preventDefault()
+        $(this).closest('.sub').removeClass('open')
+    })
 
-	$('#sidebar').on('click', 'a', function(e)
-	{
-		e.preventDefault()
+    $('#controls_top').on('click', '.sub > a:first', function(e)
+    {
+        e.preventDefault()
+        $(this).parent().toggleClass('open')
+        $(this).next('ul').css({
+            scale: 0.8,
+            opacity: 0
+        }).animate({
+            scale: 1,
+            opacity: 1
+        }, 200)
+    })
 
-		var section = $(this).closest('section').data('section')
-		var elems
-		var torrents_list = $('#torrents').children('.content').children()
-		var selector = ''
+    $('#sidebar').on('click', 'a', function(e)
+    {
+        e.preventDefault()
 
-		switch(section)
-		{
-			case 'torrents':
-				selector = $(this).data('show')
-				if(selector.length <= 0)
-					selector = ''
+        var section = $(this).closest('section').data('section')
+        var elems
+        var torrents_list = $('#torrents').children('.content').children()
+        var selector = ''
 
-				break
+        switch(section)
+        {
+            case 'torrents':
+                selector = $(this).data('show')
+                if(selector.length <= 0)
+                    selector = ''
 
-			case 'labels':
-				var label = $(this).data('label')
-				selector = '[data-label=' + label + ']'
-				
-				break
-		}
+                break
 
-		elems = selector.length > 0 ? torrents_list.filter(selector) : torrents_list
-		
-		torrents_list.not(elems).animate({
-			scale: 0.9,
-			opacity: 0,
-			height: 0
-		}, 150, function()
-		{
-			$(this).addClass('hidden')
-		})
+            case 'labels':
+                var label = $(this).data('label')
+                selector = '[data-label=' + label + ']'
+                
+                break
+        }
 
-		elems.css('height', 75).removeClass('hidden').animate({
-			opacity: 1,
-			scale: 1
-		}, 150)
-	})
+        elems = selector.length > 0 ? torrents_list.filter(selector) : torrents_list
+        
+        torrents_list.not(elems).animate({
+            scale: 0.9,
+            opacity: 0,
+            height: 0
+        }, 150, function()
+        {
+            $(this).addClass('hidden')
+        })
+
+        elems.css('height', 75).removeClass('hidden').animate({
+            opacity: 1,
+            scale: 1
+        }, 150)
+    })
 })
