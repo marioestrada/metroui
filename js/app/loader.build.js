ec9b4a21822b49428c4cd639e98fe30f1c6b67ad
Mario Estrada <mario@bittorrent.com>
2012-09-19 13:28:04 -0700

    deleted some unused stuff

--- js/app/loader.build.js
+++
@@ -1,8 +0,0 @@
-/* var exports = {}; */
-var app_version = '{version}';
-var cb = '?' + app_version;
-
-$LAB
-	.script('js/build/vendor.min.js' + cb).wait()
-	.script('js/build/talon.min.js' + cb).wait()
-	.script('js/build/app.min.js' + cb);
